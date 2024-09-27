90795da8776fe760f4c5a1c3934629ac16f6249e
nikhilrana13 <nikhilrajput060@gmail.com>
2024-09-27 15:38:22 +0530

    prepare for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ YouTube-Video-Fetcher-Using-react.js/',
   plugins: [react()],
 })
